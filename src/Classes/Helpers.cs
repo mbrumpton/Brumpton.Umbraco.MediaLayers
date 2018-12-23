7ee5ad73f51704c9251a873361d2b4aee97c9cde
Matthew Brumpton <mbrumpton@hotmail.com>
2018-12-23 20:37:37 +0000

    First commit.

---
+++ src/Classes/Helpers.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using umbraco;
+using umbraco.NodeFactory;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+
+namespace MediaLayers.Umbraco.Classes
+{
+    public class Helpers
+    {
+        #region public methods
+
+        public static List<Node> FindChildren(Node currentNode, Func<Node, bool> predicate)
+        {
+            var result = new List<Node>();
+
+            var nodes = currentNode
+                .Children
+                .OfType<Node>()
+                .Where(predicate).ToList();
+
+            if (nodes.Count() != 0)
+                result.AddRange(nodes);
+
+            foreach (var child in currentNode.Children.OfType<Node>())
+            {
+                nodes = FindChildren(child, predicate);
+                if (nodes.Count() != 0)
+                    result.AddRange(nodes);
+            }
+            return result;
+        }
+
+        public static List<Node> FindParent(Node currentNode, Func<Node, bool> predicate)
+        {
+            return currentNode.GetAncestorOrSelfNodes().Where(predicate).ToList();
+        }
+
+        public static string GetMediaItemsFromCsvId(string csvMediaIds)
+        {
+            UmbracoHelper uHelper = new UmbracoHelper(UmbracoContext.Current);
+            string mediaUrl = "";
+  
+                var mediaItem = uHelper.Media(csvMediaIds);
+                mediaUrl = mediaItem.umbracoFile;
+  
+            return mediaUrl;
+        }
+
+
+        /// <summary>
+        /// ReplaceSmartQuotesEtcInString method - usage as name implies.
+        /// </summary>
+        /// <param name="stringToConvert">The string to replace smart quotes etc in.</param>
+        /// <returns>
+        /// The converted string.
+        /// </returns>
+        public string ReplaceSmartQuotesEtcInString(string stringToConvert)
+        {
+
+            var convertedString = stringToConvert;
+
+            // Replace string â€˜ (left smart quote) with apostrophe
+            convertedString = convertedString.Replace("â€˜", "'");
+            // Replace string â€™ (right smart quote) with apostrophe
+            convertedString = convertedString.Replace("â€™", "'");
+
+            // Replace string â€œ (left double quote) with quote
+            convertedString = convertedString.Replace("â€œ", "\"");
+            // Replace string â€ (right double quote) with quote
+            convertedString = convertedString.Replace("â€	", "\"");
+
+            // Replace string â€“ (emdash) with hyphen
+            convertedString = convertedString.Replace("â€“", "-");
+            // Replace string â€” (emdash) with hyphen
+            convertedString = convertedString.Replace("â€”", "-");
+
+            // Replace string â€‹ (whatever this is supposed to be) with nothing
+            convertedString = convertedString.Replace("â€‹", "");
+
+            return convertedString;
+        }
+
+        public static string GetImageUrl(int imageId)
+        {
+            IMediaService mediaService = ApplicationContext.Current.Services.MediaService;
+            var mediaItem = mediaService.GetById(imageId);
+            var serializer = new JavaScriptSerializer();
+            dynamic jsonObject = serializer.Deserialize<ImagePathModel>(mediaItem.GetValue<string>("umbracoFile"));
+
+            return jsonObject.src;
+        }
+
+        #endregion public methods
+
+    }
+
+    public class ImagePathModel
+    {
+        public string src { get; set; }
+    }
+}
---
+++ src/Models/MediaItemBO.cs
@@ -0,0 +1,43 @@
+﻿namespace MediaLayers.Umbraco.Models
+{
+    public class MediaItemBO
+    {
+        public string Name { get; set; }
+
+        public string TitleName { get; set; }
+
+        public string NcContentTypeAlias { get; set; }
+
+        public string YoutubeName { get; set; }
+
+        public string YouTubeID { get; set; }
+
+        public string YoutubePlaylistName { get; set; }
+
+        public string YouTubePlaylistID { get; set; }
+
+        public string Text { get; set; }
+
+        public string Image { get; set; }
+
+        public string SingleImageCaption { get; set; }
+
+        public string SingleImageName { get; set; }
+
+        public string GalleryImage { get; set; }
+
+        public string GalleryName { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string PersonIntroduction { get; set; }
+
+        public string PersonBiography { get; set; }
+
+        public string PersonImage { get; set; }
+
+        public string CollapseLabel { get; set; }
+
+        public string CollapseValue { get; set; }
+    }
+}
